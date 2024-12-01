54aeaa85859580972c8430a0465ecf4b735e8753
Arman <arman.ghbn@gmail.com>
2024-12-01 20:40:16 +0330

    add loss to init for proper import

--- gorch/nn/modules/__init__.py
+++ gorch/nn/modules/__init__.py
@@ -1,9 +1,12 @@
 from .module import Module as Module
 from .linear import Linear as Linear
 from .loss import MSELoss as MSELoss
-
+from .loss import CrossEntropyLoss as CrossEntropyLoss
+from .loss import BCELoss as BCELoss
 __all__ = [
     "Module",
     "Linear",
-    "MSELoss"
+    "MSELoss",
+    "CrossEntropyLoss",
+    "BCELoss"
 ]
