0dd1b710f728fdedbb8a57723ae61464ffab8554
Arman <arman.ghbn@gmail.com>
2024-12-01 18:46:49 +0330

    fix the forward arguement problem

--- gorch/nn/activation.py
+++ gorch/nn/activation.py
@@ -1,36 +1,36 @@
 from .modules.module import Module
-
+import gorch
 class Sigmoid(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, *inputs, **kwargs):
-        return inputs.sigmoid()
+    def forward(self, inputs, **kwargs):
+        return gorch.sigmoid(inputs)
         
 class Relu(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, *inputs, **kwargs):
-        return inputs.relu()
+    def forward(self, inputs, **kwargs):
+        return gorch.relu(inputs)
         
 class Tanh(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, *inputs, **kwargs):
-        return inputs.tanh()
+    def forward(self, inputs, **kwargs):
+        return gorch.tanh(inputs)
       
 class Softmax(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, *inputs, **kwargs):
-        return inputs.softmax()
+    def forward(self, inputs, **kwargs):
+        return gorch.softmax(inputs)
     
 class LeakyRelu(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, *inputs, **kwargs):
-        return inputs.leakyRelu()
+    def forward(self, inputs, **kwargs):
+        return gorch.leakyRelu(inputs)
