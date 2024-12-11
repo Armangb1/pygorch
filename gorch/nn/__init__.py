e9c4f73217eb6668a3d3ddeeab7f5a0366fd7801
Arman <arman.ghbn@gmail.com>
2024-12-11 14:42:49 +0330

    add funnctional module to init

--- gorch/nn/__init__.py
+++ gorch/nn/__init__.py
@@ -3,3 +3,5 @@ from .parameter import Parameter
 from .activation import Sigmoid as Sigmoid
 from .activation import Tanh as Tanh
 from .activation import Relu as Relu
+from .activation import Softmax as Softmax
+from .functional import *
