173615d792325ce002e5a50506ea6dddd93f4ac5
Arman <arman.ghbn@gmail.com>
2024-12-11 14:35:10 +0330

    add bias option in arguement

--- gorch/nn/modules/linear.py
+++ gorch/nn/modules/linear.py
@@ -1,13 +1,16 @@
 from .module import Module
 from ..parameter import Parameter
-
+import gorch
 class Linear(Module):
-    def __init__(self, in_dim, out_dim) -> None:
+    def __init__(self, in_dim, out_dim, bias:bool = True) -> None:
         super().__init__()
         self.weight = Parameter((in_dim, out_dim))
-        self.bias = Parameter((1, out_dim))
         self.add_parameter('Weight', self.weight)
-        self.add_parameter('bias', self.bias)
+        if bias:
+            self.bias = Parameter((1, out_dim))
+            self.add_parameter('bias', self.bias)
+        else:
+            self.bias = gorch.Tensor([[0]])
 
     def forward(self, inputs, **kwargs):
         out = inputs@self.weight+self.bias
